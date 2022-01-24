f6fe1b4971766eb7a7772f0d90beb10cf70865c9
Alex Kiura <kiuraalex@gmail.com>
2022-01-25 01:18:58 +0300

    Add bad key and supported verbs

--- db/constants.py
+++ db/constants.py
@@ -1,5 +1,10 @@
+from tarfile import SUPPORTED_TYPES
+
+
 BAD_ARGS = 'Bad arguments provided. Refer to usage for allowed arguments'
 
 BAD_VERB = 'Bad verb provided. Refer to usage supported verbs'
 
-BAD_KEY = ''
+BAD_KEY = 'Provided key does not exist'
+
+SUPPORTED_VERBS = {'get', 'set', 'delete'}
