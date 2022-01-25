97865739398f1c8029ff96355ef565f5a0d11aa1
Alex Kiura <kiuraalex@gmail.com>
2022-01-26 01:20:31 +0300

    Parse arguments for processing

--- db/tool.py
+++ db/tool.py
@@ -1,4 +1,16 @@
+from constants import BAD_ARGS, SUPPORTED_VERBS, BAD_VERB
+
+
+def usage():
+    pass
+
+
 def main(argv):
     if not (4 <= len(argv) <= 5):
         usage()
         return BAD_ARGS
+    dbname, verb, key, value = (argv[1:] + [None])[:4]
+    if verb not in SUPPORTED_VERBS:
+        usage()
+        return BAD_VERB
+    db = db.connect(dbname)
