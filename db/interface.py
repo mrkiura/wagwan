603f7da71d212ebeaabf8972cd736fff60c5b8f5
Alex Kiura <kiuraalex@gmail.com>
2022-02-01 13:43:19 +0300

    Initialize database object

--- db/interface.py
+++ db/interface.py
@@ -1,2 +1,8 @@
-class SimpleDB:
-    pass
+from db.storage import Storage
+from db.binary_tree import BinaryTree
+
+class SimpleDB(object):
+
+    def __init__(self) -> None:
+        self._storage = Storage()
+        self._tree = BinaryTree(self._storage)
