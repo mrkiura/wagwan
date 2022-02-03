c75c7fa84e5f59ccbdc48bba474d63ff86c9dde1
Alex Kiura <kiuraalex@gmail.com>
2022-02-03 08:47:24 +0300

    Implement get item

--- db/interface.py
+++ db/interface.py
@@ -6,3 +6,10 @@ class SimpleDB(object):
     def __init__(self) -> None:
         self._storage = Storage()
         self._tree = BinaryTree(self._storage)
+
+    def __getitem__(self, key):
+        self._assert_not_closed()
+        return self._tree.get(key)
+
+    def _assert_not_closed(self):
+        assert self._storage.closed, 'Database closed'
