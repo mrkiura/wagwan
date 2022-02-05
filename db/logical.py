ac1de85de790c6b1f5c03a19bcb20060fbe1ad59
Alex Kiura <kiuraalex@gmail.com>
2022-02-05 14:16:28 +0300

    Implement get and update tree reference

---
+++ db/logical.py
@@ -0,0 +1,8 @@
+class LogicalBase(object):
+    def get(self, key):
+        if not self._storage.locked:
+            self._refresh_tree_ref()
+        return self._get(self._follow(self._tree_ref), key)
+
+    def _refresh_tree_ref(self):
+        self._tree_ref = self.node_ref_class(address=self._storage.get_root_address())
