9b4a424faa85e826e1f4dbc6e599ae57de492066
Alex Kiura <kiuraalex@gmail.com>
2022-02-07 12:21:18 +0300

    Update function definition with docstrings

--- db/logical.py
+++ db/logical.py
@@ -1,8 +1,12 @@
 class LogicalBase(object):
     def get(self, key):
+        """Retrieve value identified with key {key}.
+        """
         if not self._storage.locked:
             self._refresh_tree_ref()
         return self._get(self._follow(self._tree_ref), key)
 
     def _refresh_tree_ref(self):
+        """Syncs tree data with what is curently on disk.
+        """
         self._tree_ref = self.node_ref_class(address=self._storage.get_root_address())
