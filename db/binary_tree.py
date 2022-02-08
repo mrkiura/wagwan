19ae1bb1c595354728f70bc8cca475bae75d0e86
Alex Kiura <kiuraalex@gmail.com>
2022-02-08 16:07:51 +0300

    Retrieve from binary tree

--- db/binary_tree.py
+++ db/binary_tree.py
@@ -1,2 +1,14 @@
-class BinaryTree(object):
-    pass
+
+from db.logical import LogicalBase
+
+
+class BinaryTree(LogicalBase):
+    def _get(self, node, key):
+        while node is not None:
+            if key < node.key:
+                node = self._follow(node.left_ref)
+            elif node.key < key:
+                node = self._follow(node.right_ref)
+            else:
+                return self._follow(node.value_ref)
+        raise KeyError
