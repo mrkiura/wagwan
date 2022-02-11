1d02163261072b3b6e57d9cc2b7ec54dcd842e55
Alex Kiura <kiuraalex@gmail.com>
2022-02-11 09:45:40 +0300

    Implement lock in storage class

--- db/storage.py
+++ db/storage.py
@@ -1,2 +1,15 @@
+import portalocker
+
+
 class Storage(object):
-    pass
+
+    def __init__(self) -> None:
+        self._file = None
+
+    def lock(self):
+        if not self.locked:
+            portalocker.lock(self._file, portalocker.LOCK_EX)
+            self.locked = True
+            return True
+        else:
+            return False
