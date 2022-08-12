56f1fae3f5d4da0775a28816b8144d711f3f60d7
Alex Kiura <kiuraalex@gmail.com>
2022-08-12 07:29:03 +0300

    Initiate BinaryNode

--- db/binary_tree.py
+++ db/binary_tree.py
@@ -50,6 +50,32 @@ class BinaryNodeRef(ValueRef):
         })
 
 class BinaryNode:
+
+    @classmethod
+    def from_node(cls, node, **kwargs):
+        length = node.length
+        if 'left_ref' in kwargs:
+            length += kwargs['left_ref'].length - node.left_ref.length
+
+        if 'right_ref' in kwargs:
+            length += kwargs['right_ref'].length - node.right_ref.length
+
+        return cls(
+            left_ref=kwargs.get('left_ref', node.left_ref),
+            key=kwargs.get('key', node.key),
+            value_ref=kwargs.get('value_ref', node.value_ref),
+            right_ref=kwargs.get('right_ref', node.right_ref),
+            length=length
+        )
+
+    def __init__(self, left_ref, key, value_ref, right_ref, length) -> None:
+        self.left_ref = left_ref
+        self.key = key
+        self.value_ref = value_ref
+        self.right_ref = right_ref
+        self.length = length
+
+
     def store_refs(self, storage):
         self.value_ref.store(storage)
         self.left_ref.store(storage)
