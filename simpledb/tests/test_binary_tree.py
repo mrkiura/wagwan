3fe3743d87f9eeacb8c3999c49c65bbb1f0eb964
Alex Kiura <kiuraalex@gmail.com>
2022-08-13 16:35:08 +0300

    Improving binary tree tests by using pytest fixtures and some pytest magic

--- simpledb/tests/test_binary_tree.py
+++ simpledb/tests/test_binary_tree.py
@@ -1,7 +1,7 @@
 import pickle
+import pytest
 import random
 
-from nose.tools import assert_raises, eq_
 
 from simpledb.binary_tree import BinaryNode, BinaryTree, BinaryNodeRef, ValueRef
 
@@ -33,90 +33,91 @@ class StubStorage(object):
         return self.d[address]
 
 
-class TestBinaryTree(object):
-    def setup(self):
-        self.tree = BinaryTree(StubStorage())
 
-    def test_get_missing_key_raises_key_error(self):
-        with assert_raises(KeyError):
-            self.tree.get("Not A Key In The Tree")
-
-    def test_set_and_get_key(self):
-        self.tree.set("a", "b")
-        eq_(self.tree.get("a"), "b")
-
-    def test_random_set_and_get_keys(self):
-        ten_k = list(range(10000))
-        pairs = list(zip(random.sample(ten_k, 10), random.sample(ten_k, 10)))
-        for i, (k, v) in enumerate(pairs, start=1):
-            self.tree.set(k, v)
-            eq_(len(self.tree), i)
-        for k, v in pairs:
-            eq_(self.tree.get(k), v)
-        random.shuffle(pairs)
-        for i, (k, v) in enumerate(pairs, start=1):
-            self.tree.pop(k)
-            eq_(len(self.tree), len(pairs) - i)
-
-    def test_overwrite_and_get_key(self):
-        self.tree.set("a", "b")
-        self.tree.set("a", "c")
-        eq_(self.tree.get("a"), "c")
-
-    def test_pop_non_existent_key(self):
-        with assert_raises(KeyError):
-            self.tree.pop("Not A Key In The Tree")
-
-    def test_del_leaf_key(self):
-        self.tree.set("b", "2")
-        self.tree.pop("b")
-        with assert_raises(KeyError):
-            self.tree.get("b")
-
-    def test_del_left_node_key(self):
-        self.tree.set("b", "2")
-        self.tree.set("a", "1")
-        self.tree.pop("b")
-        with assert_raises(KeyError):
-            self.tree.get("b")
-        self.tree.get("a")
-
-    def test_del_right_node_key(self):
-        self.tree.set("b", "2")
-        self.tree.set("c", "3")
-        self.tree.pop("b")
-        with assert_raises(KeyError):
-            self.tree.get("b")
-        self.tree.get("c")
-
-    def test_del_full_node_key(self):
-        self.tree.set("b", "2")
-        self.tree.set("a", "1")
-        self.tree.set("c", "3")
-        self.tree.pop("b")
-        with assert_raises(KeyError):
-            self.tree.get("b")
-        self.tree.get("a")
-        self.tree.get("c")
+@pytest.fixture
+def tree():
+    return BinaryTree(StubStorage())
 
 
-class TestBinaryNodeRef(object):
-    def test_to_string_leaf(self):
-        n = BinaryNode(BinaryNodeRef(), "k", ValueRef(address=999), BinaryNodeRef(), 1)
-        pickled = BinaryNodeRef.referent_to_string(n)
-        d = pickle.loads(pickled)
-        eq_(d["left"], 0)
-        eq_(d["key"], "k")
-        eq_(d["value"], 999)
-        eq_(d["right"], 0)
+def test_get_missing_key_raises_key_error(tree):
+    with pytest.raises(KeyError):
+        tree.get("Not A Key In The Tree")
 
-    def test_to_string_nonleaf(self):
-        left_ref = BinaryNodeRef(address=123)
-        right_ref = BinaryNodeRef(address=321)
-        n = BinaryNode(left_ref, "k", ValueRef(address=999), right_ref, 3)
-        pickled = BinaryNodeRef.referent_to_string(n)
-        d = pickle.loads(pickled)
-        eq_(d["left"], 123)
-        eq_(d["key"], "k")
-        eq_(d["value"], 999)
-        eq_(d["right"], 321)
+def test_set_and_get_key(tree):
+    tree.set("a", "b")
+    assert tree.get("a") ==  "b"
+
+def test_random_set_and_get_keys(tree):
+    ten_k = list(range(10000))
+    pairs = list(zip(random.sample(ten_k, 10), random.sample(ten_k, 10)))
+    for i, (k, v) in enumerate(pairs, start=1):
+        tree.set(k, v)
+        assert len(tree) == i
+    for k, v in pairs:
+        assert tree.get(k) == v
+    random.shuffle(pairs)
+    for i, (k, v) in enumerate(pairs, start=1):
+        tree.pop(k)
+        assert len(tree) == len(pairs) - i
+
+def test_overwrite_and_get_key(tree):
+    tree.set("a", "b")
+    tree.set("a", "c")
+    assert tree.get("a") == "c"
+
+def test_pop_non_existent_key(tree):
+    with pytest.raises(KeyError):
+        tree.pop("Not A Key In The Tree")
+
+def test_del_leaf_key(tree):
+    tree.set("b", "2")
+    tree.pop("b")
+    with pytest.raises(KeyError):
+        tree.get("b")
+
+def test_del_left_node_key(tree):
+    tree.set("b", "2")
+    tree.set("a", "1")
+    tree.pop("b")
+    with pytest.raises(KeyError):
+        tree.get("b")
+    tree.get("a")
+
+def test_del_right_node_key(tree):
+    tree.set("b", "2")
+    tree.set("c", "3")
+    tree.pop("b")
+    with pytest.raises(KeyError):
+        tree.get("b")
+    tree.get("c")
+
+def test_del_full_node_key(tree):
+    tree.set("b", "2")
+    tree.set("a", "1")
+    tree.set("c", "3")
+    tree.pop("b")
+    with pytest.raises(KeyError):
+        tree.get("b")
+    tree.get("a")
+    tree.get("c")
+
+
+def test_to_string_leaf():
+    n = BinaryNode(BinaryNodeRef(), "k", ValueRef(address=999), BinaryNodeRef(), 1)
+    pickled = BinaryNodeRef.referent_to_string(n)
+    d = pickle.loads(pickled)
+    assert d["left"] == 0
+    assert d["key"] == "k"
+    assert d["value"] == 999
+    assert d["right"] == 0
+
+def test_to_string_nonleaf():
+    left_ref = BinaryNodeRef(address=123)
+    right_ref = BinaryNodeRef(address=321)
+    n = BinaryNode(left_ref, "k", ValueRef(address=999), right_ref, 3)
+    pickled = BinaryNodeRef.referent_to_string(n)
+    d = pickle.loads(pickled)
+    assert d["left"] == 123
+    assert d["key"] == "k"
+    assert d["value"] == 999
+    assert d["right"] == 321
